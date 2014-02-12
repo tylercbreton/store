b209ec1a75a6b6a05539e1e2e3671b9eeb925c5d
tylercbreton <tcbreton@gmail.com>
2014-02-11 19:46:45 -0800

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Store::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
   resources :products
   resources :index
   # The priority is based upon order of creation: first created -> highest priority.
